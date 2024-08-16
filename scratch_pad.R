7b47cbddbad5888fc4ce19d5c1ea366db5b305c6
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2024-08-15 23:40:40 -0230

    Major: added FSA library and did CS estimates for various subsets.  I compared this to AMEC function to figure out how AMEC calculated biomass/abundance.

--- scratch_pad.R
+++ scratch_pad.R
@@ -10,10 +10,12 @@ amec.efishing8cs("data/test1") # this gives completely different output - not ev
 
 amec.efishing8cs("data/test2")# then, BT data but without sweeps 4 and 5. Some of these are close are the same but SITE 5 is very different.
 
+# re-running this because something very wonky with the output ito SITE 8
+amec.efishing8cs("data/year_summaries/Pamehac_2016_by_station")
+
 
 # tabulate ----
 # this is to tabulate the number of fish per pass by year and species - trying to figure out why Pamehac produces wild CIs when RB doesn't
-
 
 df_tab <- output_pamehac_by_station[, c(1:2, 3:4, 6)]
 str(df_tab)
@@ -66,3 +68,51 @@ View(dft[dft$bm_var > 100 & !is.na(dft$bm_var),])
 df_abun[df_abun$abundance.caught >= 30 & !is.na(df_abun$abundance.caught), ]
 length(df_abun$abundance.caught[df_abun$abundance.caught <= 5 & !is.na(df_abun$abundance.caught)])/(20*12) # > 30 = 11, > 20 = 17, > 10 = 29, > 5 = 0.4
 
+# FSA ----
+library(FSA)
+library(dplyr)
+pm90 <- read.csv("data/year_summaries/Pamehac_1990_by_station.csv")
+
+# originally did this with biomass function removal only works on abundance
+pm90_sum <- pm90 |>
+  group_by(Species, Station, Sweep) |>
+  summarise(bio.sum = sum(Weight.g), abun = n()) 
+pm90_sum |> print(n = Inf)
+
+# removal for one species and station
+pm90_as <- pm90_sum |>
+  ungroup() |>
+  filter(Species == "AS" & Station == 6) |>
+  select(Sweep, bio.sum, abun)
+pm90_as
+
+
+removal(pm90_as$bio.sum, method = "CarleStrub", just.ests = T)
+removal(pm90_as$abun, method = "CarleStrub", just.ests = T)
+
+# removal for one species and all stations
+library(tidyr)
+pm90_tab <- pm90_sum |>
+  filter(Species == "AS") |>
+  pivot_wider(id_cols = c(Species, Station), 
+              names_from = Sweep, values_from = bio.sum)
+  #pivot_wider(id_cols = c(Species, Station),
+   #           names_from = Sweep, values_from = abun)
+
+apply(pm90_tab[c(1, 3), c(-1:-2)], MARGIN=1, FUN = removal, method = "CarleStrub", just.ests = T)
+
+
+# removal for all species and all stations
+pm90_tab1 <- pm90_sum |>
+  pivot_wider(id_cols = c(Species, Station), 
+              names_from = Sweep, values_from = ) #bio.sum
+
+res <- apply(pm90_tab1[c(1, 3, 5:7, 9:13, 16:17), c(-1:-2)], MARGIN=1, FUN = removal, method = "CarleStrub", just.ests=T)
+
+res <- data.frame(spp = pm90_tab1$Species[c(1, 3, 5:7, 9:13, 16:17)],
+                  sta = pm90_tab1$Station[c(1, 3, 5:7, 9:13, 16:17)], 
+                  t(res))
+res
+res[res$spp == "AS",]
+
+# END ----
