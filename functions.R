1c29cd190712a3956b08363cef0dcdc9be2cdcd0
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2024-08-29 10:17:27 -0230

    Medium: modify to properly calculate sum of previous catch but this only works when data are good, i.e,. 3 sweeps with fish in each one

--- functions.R
+++ functions.R
@@ -1,14 +1,26 @@
 spc <- function(x){
   #browser()
-    for(i in seq_along(x$Sweep)){
+  for(i in seq_along(x$Sweep)){
     if(x$Sweep[i] == 1){
       x$spc[i] <- 0
     } else if(x$Sweep[i] == 2) {
       #  z[i] <- cumsum(x[i-1])
       x$spc[i] <- x$abun[i-1]
     } else {
-      x$spc[i] <- sum(x$abun[(i-2):(i-1)])
+#      x$spc[i] <- sum(x$abun[(i-2):(i-1)])
+      x$spc[i] <- sum(x$abun[(i-(i-1)):(i-1)])
     }
     }
+  return(x)     
+}
+
+
+spc1 <- function(var){
+  #browser()
+  for(i in seq_along(x$var)){
+    if(x$var[i] == 1){
+      x$spc[i] <- 0
+    }
+  }
   return(x)     
 }
