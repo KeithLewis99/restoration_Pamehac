2bebe41b0c5bfe806a2b2f992de1b5631546d745
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2024-10-23 16:01:33 -0230

    Impt: add git recipe

--- Pam_data.R
+++ Pam_data.R
@@ -1,5 +1,29 @@
+# Set up a project - see the below link for directions.
+#https://happygitwithr.com/rstudio-git-github.html
+
+# But basically:
+# 1.	Set up a Git repo on GitHub.
+# 2.	Create the project in R - New Project - Version Control - Git
+# 3. type "git add -A" in the terminal
+# 4.	Create a bunch of directories automatically (see below)
+# 5. Copy git -ignore file
+
+#Create a "name_dat.R" file
+#put this file in the folder with the project and create the following subfolders
+if(!dir.exists("archive"))dir.create("archive")
+if(!dir.exists("data"))dir.create("data")
+if(!dir.exists("data_derived"))dir.create("data_derived")
+if(!dir.exists("figs"))dir.create("figs") #for publication quality only
+if(!dir.exists("output"))dir.create("output") # for tables and figures
+if(!dir.exists("ms"))dir.create("ms") # manuscript
+if(!dir.exists("report"))dir.create("report") #for rmd report
+if(!dir.exists("refs"))dir.create("refs") #for rmd report
+
+
 # Created 2024-07-09
+
 ## This is just a compliation of Kristin Loughlin's code
+# libraries ----
 library(readr)
 library(plyr)
 library(dplyr)
