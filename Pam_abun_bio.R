4a609e34172d04a9253942fe2bf396c6ffe0d830
KeithLewis99 <Keith.Lewis@dfo-mpo.gc.ca>
2024-11-29 10:05:22 -0330

    Minor but large: just commented out a bunch of code that is more or less deprecated

--- Pam_abun_bio.R
+++ Pam_abun_bio.R
@@ -54,29 +54,29 @@ out <- cbind(year = df_tab1$Year,
 
 #View(out)
 head(out)
-out |> filter(spp == "AS" & year == 2016)
-
-
-### GF calc ----
-out$GF <- with(out, round((c1 - (No*p))^2/No*p + 
-                            (c2 - No*(1-p)*p)^2/(No*(1-p)*p) +
-                            (c3 - (No*(1-p)^2*p))^2/(No*(1-p)^2*p)
-                          ,4)             
-)
-str(out)
-write.csv(out, "derived_data/FSA_output.csv")
-dchisq(1.2813, 2) ## What is the likelihood of this value
-pchisq(1.2813, 2) # probability of this value or less - cumulative density
-1-pchisq(1.2813, 2) #- this is the pvalue
-qchisq(1-0.5269498, 2) #- returns the test statistic
-qchisq(0.95, 1) #- gives the critical test 3.84 - this is right - it should be one df because k-2: 2 df are lost because N is estimated - see Locwood2000
-
-
-# filter out sites with only 1 catch or where c2 & c3 == NA
-nrow(out)
-out |> filter(GF > qchisq(0.95, 1)) # 10 sites don't make GF with T > 30 on 5 sites 
-nrow(out |> filter(GF > qchisq(0.95, 1)))
-out |> filter(T < 30)
+# out |> filter(spp == "AS" & year == 2016)
+# 
+# 
+# ### GF calc ----
+# out$GF <- with(out, round((c1 - (No*p))^2/No*p + 
+#                             (c2 - No*(1-p)*p)^2/(No*(1-p)*p) +
+#                             (c3 - (No*(1-p)^2*p))^2/(No*(1-p)^2*p)
+#                           ,4)             
+# )
+# str(out)
+# write.csv(out, "derived_data/FSA_output.csv")
+# dchisq(1.2813, 2) ## What is the likelihood of this value
+# pchisq(1.2813, 2) # probability of this value or less - cumulative density
+# 1-pchisq(1.2813, 2) #- this is the pvalue
+# qchisq(1-0.5269498, 2) #- returns the test statistic
+# qchisq(0.95, 1) #- gives the critical test 3.84 - this is right - it should be one df because k-2: 2 df are lost because N is estimated - see Locwood2000
+# 
+# 
+# # filter out sites with only 1 catch or where c2 & c3 == NA
+# nrow(out)
+# out |> filter(GF > qchisq(0.95, 1)) # 10 sites don't make GF with T > 30 on 5 sites 
+# nrow(out |> filter(GF > qchisq(0.95, 1)))
+# out |> filter(T < 30)
 nrow(out |> filter(T < 30)) # 97 of 124
 nrow(out |> filter(T < 20)) # 85 of 124
 nrow(out |> filter(T < 10)) # 57 of 124
@@ -84,72 +84,72 @@ nrow(out |> filter(T < 5)) # 33 of 124
 
 # density of total catch
 plot(density(out$T))
-
-
+ 
+ 
 p <- ggplot(out, aes(x = T, y = No, group = as.factor(spp), colour = spp)) +
   geom_point()
 p
-
-
-## GF - pool ----
-res_list_pool <- apply(df_tab_pool[, c(3:5)], MARGIN=1, FUN = removal, method = "CarleStrub") # takes NA's
-
-
-# this works but only when three catches so maybe that is fine - filter above on this.
-out_pool <- as.data.frame(matrix(NA, length(res_list_pool), 11))
-colnames(out_pool) <- c("c1",  "c2", "c3","k",  "T", "X", 
-                        "No",  "No.se", "No.LCI", 
-                        "No.UCI", "p"
-)
-
-
-for(i in seq_along(res_list_pool)){
-  if(length(res_list_pool[[i]]$catch) ==3){
-    out_pool[i,] <- round(c(res_list_pool[[i]]$catch, 
-                            res_list_pool[[i]]$int, 
-                            res_list_pool[[i]]$est[1:5]), 2)
-    
-  } else if (names(res_list_pool[[i]]$catch[2]) == 2 & 
-             length(res_list_pool[[i]]$catch) == 2){
-    out_pool[i,c(1:2, 4:11)] <- round(c(res_list_pool[[i]]$catch, 
-                                        res_list_pool[[i]]$int, 
-                                        res_list_pool[[i]]$est[1:5]), 2)
-  } else if (names(res_list_pool[[i]]$catch[2]) == 3 & 
-             length(res_list_pool[[i]]$catch) == 2){
-    out_pool[i,c(1, 3:11)] <- round(c(res_list_pool[[i]]$catch, 
-                                      res_list_pool[[i]]$int, 
-                                      res_list_pool[[i]]$est[1:5]), 2)
-  }
-}
-
-
-out_pool <- cbind(year = df_tab_pool$Year, 
-                  sta = df_tab_pool$Station, 
-                  out_pool)
-
-#View(out_pool)
-head(out_pool)
-
-
-out_pool$GF <- with(out_pool, round((c1 - (No*p))^2/No*p + 
-                                      (c2 - No*(1-p)*p)^2/(No*(1-p)*p) +
-                                      (c3 - (No*(1-p)^2*p))^2/(No*(1-p)^2*p)
-                                    ,4)             
-)
-
-out_pool
-qchisq(0.95, 1) #- gives the critical test 3.84
-
-
-# for pooled
-# filter out sites with only 1 catch or where c2 & c3 == NA
-nrow(out_pool)
-out_pool |> filter(GF > qchisq(0.95, 1)) # 6 sites don't make GF with T > 30 on 3 sites 
-nrow(out_pool |> filter(GF > qchisq(0.95, 1)))
-out_pool |> filter(T < 30)
-nrow(out_pool |> filter(T < 30)) # 19 of 45
-nrow(out_pool |> filter(T < 20)) # 12 of 45
-nrow(out_pool |> filter(T < 10)) # 6 of 45
+# 
+# 
+# ## GF - pool ----
+# res_list_pool <- apply(df_tab_pool[, c(3:5)], MARGIN=1, FUN = removal, method = "CarleStrub") # takes NA's
+# 
+# 
+# # this works but only when three catches so maybe that is fine - filter above on this.
+# out_pool <- as.data.frame(matrix(NA, length(res_list_pool), 11))
+# colnames(out_pool) <- c("c1",  "c2", "c3","k",  "T", "X", 
+#                         "No",  "No.se", "No.LCI", 
+#                         "No.UCI", "p"
+# )
+# 
+# 
+# for(i in seq_along(res_list_pool)){
+#   if(length(res_list_pool[[i]]$catch) ==3){
+#     out_pool[i,] <- round(c(res_list_pool[[i]]$catch, 
+#                             res_list_pool[[i]]$int, 
+#                             res_list_pool[[i]]$est[1:5]), 2)
+#     
+#   } else if (names(res_list_pool[[i]]$catch[2]) == 2 & 
+#              length(res_list_pool[[i]]$catch) == 2){
+#     out_pool[i,c(1:2, 4:11)] <- round(c(res_list_pool[[i]]$catch, 
+#                                         res_list_pool[[i]]$int, 
+#                                         res_list_pool[[i]]$est[1:5]), 2)
+#   } else if (names(res_list_pool[[i]]$catch[2]) == 3 & 
+#              length(res_list_pool[[i]]$catch) == 2){
+#     out_pool[i,c(1, 3:11)] <- round(c(res_list_pool[[i]]$catch, 
+#                                       res_list_pool[[i]]$int, 
+#                                       res_list_pool[[i]]$est[1:5]), 2)
+#   }
+# }
+# 
+# 
+# out_pool <- cbind(year = df_tab_pool$Year, 
+#                   sta = df_tab_pool$Station, 
+#                   out_pool)
+# 
+# #View(out_pool)
+# head(out_pool)
+# 
+# 
+# out_pool$GF <- with(out_pool, round((c1 - (No*p))^2/No*p + 
+#                                       (c2 - No*(1-p)*p)^2/(No*(1-p)*p) +
+#                                       (c3 - (No*(1-p)^2*p))^2/(No*(1-p)^2*p)
+#                                     ,4)             
+# )
+# 
+# out_pool
+# qchisq(0.95, 1) #- gives the critical test 3.84
+# 
+# 
+# # for pooled
+# # filter out sites with only 1 catch or where c2 & c3 == NA
+# nrow(out_pool)
+# out_pool |> filter(GF > qchisq(0.95, 1)) # 6 sites don't make GF with T > 30 on 3 sites 
+# nrow(out_pool |> filter(GF > qchisq(0.95, 1)))
+# out_pool |> filter(T < 30)
+# nrow(out_pool |> filter(T < 30)) # 19 of 45
+# nrow(out_pool |> filter(T < 20)) # 12 of 45
+# nrow(out_pool |> filter(T < 10)) # 6 of 45
 
 
 # density of total catch
@@ -161,5 +161,8 @@ p <- ggplot(out_pool, aes(x = T, y = No)) +
 p
 
 # tabulations ----
+table(out$year, out$spp, out$T)
+
+
 
 # END ----
