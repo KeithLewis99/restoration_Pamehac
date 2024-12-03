1a0a78e62fc3f45bb094f69292ed2b2281af858d
KeithLewis99 <Keith.Lewis@dfo-mpo.gc.ca>
2024-12-02 21:20:05 -0330

    Med: start of glmm for bt

--- Pam_analyses.R
+++ Pam_analyses.R
@@ -8,31 +8,31 @@ source("Pam_abun_bio.R")
 # See ReadMe for thoughts on Pamehac. 
 
 # library ----
-library(nlme)
+#library(nlme)
 library(glmmTMB)
 library(DHARMa)
-library(readr)
-library(tidyr)
+#library(readr)
+#library(tidyr)
 library(ggplot2)
 library(cowplot)
 
 
 # data ----
-str(dft, give.attr=FALSE)
+str(df_a, give.attr=FALSE)
 df_aBT <- df_a[df_a$Species == "BT",]
 df_aBT$int <- interaction(df_aBT$type, df_aBT$time)
-plot(density(df_aBT$bm))
-summary(dft_BT$bm)
+plot(density(df_aBT$abun.stand, na.rm = T))
+summary(df_aBT$abun.stand)
 
 
 ### Need glmmTMB to have random effects and a non-normal distribution
 #### Gamma is OK here because there are no zeros
 bt.glmm1 <- glmmTMB(
-  bm ~ as.factor(type) + as.factor(time) + (1 | Year),
+  abun.stand ~ type*time + (1 | Year),
   dispformula = ~ int,
   family=ziGamma(link="log"), ziformula = ~1,
   REML = TRUE,
-  data = dft_BT
+  data = df_aBT
 )
 
 summary(bt.glmm1)
@@ -44,32 +44,29 @@ summary(bt.glmm1)
 ### glmmTMB to glm.  The estimates are virtually identical but the Std. Errors are much smaller for glmmTMB.
 ### PLOTS/RESULTS WITH AND WITHOUT DISPERSION FOR THE GLMMTMB.  
 bt.glmm2 <- glmmTMB(
-  bm ~ as.factor(type) + as.factor(time) + (1 | Year),
+  abun.stand ~ type*time + (1 | Year),
   family=ziGamma(link="log"), ziformula = ~1,
   REML = TRUE,
-  data = dft_BT
+  data = df_aBT
 )
 summary(bt.glmm2)
 
 # str(bt.glmm2)
 anova(bt.glmm1, bt.glmm2) # this suggests that model bt.glmm2 without dispersion is better than with (bt.glmm1)
 
-## The estimates are virtually identical but the Std. Errors are slightly smaller than the model with dispersion.  
-#Further, the diagnostics look better much better for this model (see below but tested separately for bt.glmm2 which is no longer in the code), especially for homogeneity of variance and this model has a much better AIC. 
-# OK, the bt.glmm1 model is best.  Now, its time to see if its valid (Fifield said no need to test the less valid model at this point)?  Talked to Fifield about how to do this.  He and glmmTMB suggest using the DHARMa package
 
 ## diagnostics ----
-bt.glmm1_simres <- simulateResiduals(bt.glmm1)
+bt.glmm2_simres <- simulateResiduals(bt.glmm2)
 # str(bt.glmm1_simres,1)
-plot(bt.glmm1_simres)
+plot(bt.glmm2_simres)
 # The normality and homogeneity of variance look great.  Fifield said that this needs to be looked at for all the usual reasons: normality and homogeneity of variance.  But what about temporal and spatial independence? 
 
 
 ### temporal independence
 # But, to look at temporal autocorrelation, we need to recalculate them with Year as a grouping variable
-bt.glmm1_simres_recalc <- recalculateResiduals(bt.glmm1_simres, group = dft_BT$Year)
-# plot(bt.glmm1_simres_recalc) Dave said that this is not required
-testTemporalAutocorrelation(bt.glmm1_simres_recalc, time = unique(dft_BT$Year))
+bt.glmm2_simres_recalc <- recalculateResiduals(bt.glmm2_simres, group = df_aBT$Year)
+
+testTemporalAutocorrelation(bt.glmm2_simres_recalc, time = unique(df_aBT$Year))
 
 # resids look great: conclude no temproal issues
 
