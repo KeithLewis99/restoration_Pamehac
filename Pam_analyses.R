d2f254e345729d23c330195817771da532d7db64
KeithLewis99 <Keith.Lewis@dfo-mpo.gc.ca>
2024-12-06 11:17:43 -0330

    Major: add AS

--- Pam_analyses.R
+++ Pam_analyses.R
@@ -28,6 +28,12 @@ plot(density(df_aBT$abun.stand, na.rm = T))
 summary(df_aBT$abun.stand)
 with(df_aBT, table(abun, Year))
 
+df_a |>
+  group_by(Year, Species) |>
+  summarise(a = sum(abun != 0), 
+            a0 = sum(abun == 0))
+
+
 ### analyses ----
 ### Need glmmTMB to have random effects and a non-normal distribution
 #### Gamma is OK here because there are no zeros
@@ -49,7 +55,7 @@ summary(bt.glmm1)
 ### PLOTS/RESULTS WITH AND WITHOUT DISPERSION FOR THE GLMMTMB.  
 bt.glmm2 <- glmmTMB(
   abun.stand ~ type*time + (1 | Year), #+ (1 | Station),
-  family=ziGamma(link="log"), ziformula = ~1,
+  family=ziGamma(link="log"), ziformula = ~,
   REML = TRUE,
   data = df_aBT
 )
@@ -216,5 +222,100 @@ mean_by_site(df_sumAS, "d")
 baci.plot(df_baciAS, "d")
 
 
+# Biomass ----
+## AS ----
+### data ----
+with(df_aAS, table(bio, Year))
 
+### analyses ----
+### Need glmmTMB to have random effects and a non-normal distribution
+#### Gamma is OK here because there are no zeros
+as_bio.glmm1 <- glmmTMB(
+  bio.stand ~ type*time + (1 | Year), #+ (1 | Station),
+  dispformula = ~ int,
+  family=ziGamma(link="log"), ziformula = ~1,
+  REML = TRUE,
+  data = df_aAS
+)
+
+summary(as_bio.glmm1)
+
+
+as_bio.glmm2 <- glmmTMB(
+  bio.stand ~ type*time + (1 | Year), #+ (1 | Station),
+  family=ziGamma(link="log"), ziformula = ~1,
+  REML = TRUE,
+  data = df_aAS
+)
+summary(as_bio.glmm2)
+
+# str(as_bio.glmm2)
+# as_bio.glmm1 does not run - probably not enough data.  Use as_bio.glmm2 but see how diagnostics look 
+
+
+### diagnostics ----
+as_bio.glmm2_simres <- simulateResiduals(as_bio.glmm2, plot = T)
+# str(as_bio.glmm1_simres,1)
+residuals(as_bio.glmm2_simres) 
+# these are scaled residuals
+# The normality/overdispersion aren't great but the  homogeneity of variance is bad.  
+
+# plots by themselves or by group
+testUniformity(as_bio.glmm2_simres)
+testQuantiles(as_bio.glmm2_simres)
+
+plotResiduals(as_bio.glmm2_simres, form = df_aAS$time)
+plotResiduals(as_bio.glmm2_simres, form = df_aAS$type)
+# these suggest that there may be an interaction
+
+
+# dispersion/zeroinflation
+testDispersion(as_bio.glmm2_simres)
+testZeroInflation(as_bio.glmm2_simres) # this gives a bogus result but clearly, from the inflation model, there is no zero-inflation.
+
+
+### temporal independence
+# But, to look at temporal autocorrelation, we need to recalculate them with Year as a grouping variable
+as_bio.glmm2_simres_recalc <- recalculateResiduals(as_bio.glmm2_simres, group = df_aAS$Year)
+
+testTemporalAutocorrelation(as_bio.glmm2_simres_recalc, time = unique(df_aAS$Year))
+
+# resids look great: conclude no temproal issues
+
+
+### spatial independence
+# recalculate resids with stations as the grouping variable
+as_bio.glmm2_simres_recalcSpace <- recalculateResiduals(as_bio.glmm2_simres, group = df_aAS$Station)
+unique(df_aAS$Station) 
+#str(as.glmm2_simres_recalcSpace)
+length(as_bio.glmm2_simres_recalcSpace$scaledResiduals)
+
+testSpatialAutocorrelation(as_bio.glmm2_simres_recalcSpace, x = unique(df_aAS$west), y = unique(df_aAS$north))
+
+spatialAutoCorrBase_fun(df_aAS, as_bio.glmm2_simres_recalcSpace)   
+
+
+as_bio.biomass.all <- spatialData_join(df_sumAS, as_bio.glmm2_simres_recalcSpace, df_loc[-c(2:3, 5:6, 9), ])
+
+
+spatialAutoCorrGG_fun(as_bio.biomass.all)
+
+
+# Diagnostics look fantastic for this model.  Proceed with this model (as.glmm1).  See glmm_anova for above but without REML which will allow for the BACI.
+
+### summary ----
+summary(as_bio.glmm2)
+mean_by_site(df_sumAS, "b")
+baci.plot(df_baciAS, "b")
+
+# use this to compare to Scruton et al. 1998
+tmp <- df_a |> group_by(Year, Species, type) |> filter(bio.stand > 0) |> summarise(mean_bio = mean(bio.stand)) 
+
+tmp <- df_a |> group_by(Year, Species, type) |> filter(bio.stand > 0) |> summarise(mean_bio = mean(bio.stand*100)) 
+
+tmp <- df_a |> group_by(Year, Species, type) |> summarise(mean_bio = mean(bio.stand*100)) 
+
+
+ggplot(tmp, aes(x = Year, y = mean_bio, group = type, fill = type)) + geom_col(position = position_dodge(width = 0.9)) +
+  facet_wrap(~Species)
 # END ----
