6cf37090332e963963521cea7486214da1ca5e48
KeithLewis99 <Keith.Lewis@dfo-mpo.gc.ca>
2025-10-07 16:55:37 -0230

    revised BACI plot

--- Pamehac-baci_plot_explained.R
+++ Pamehac-baci_plot_explained.R
@@ -3,7 +3,7 @@
 df <- data.frame(
   time = c("A", "A", "B", "B"), # after - before
   trt = c("B", "A", "B", "A"), # above - below
-  rv = c(5, 2, 5, 2)
+  rv = c(5, 3, 5, 2)
 )
 df
 
@@ -12,20 +12,20 @@ ggplot(df, aes(x = reorder(time, desc(time)), y = rv)) +
   geom_point() +
   geom_segment(aes(x = 1, xend = 2, y = 5, yend = 5), 
                color = "blue", linetype = "dashed", size = 1) +  # First horizontal segment
-  geom_segment(aes(x = 1, xend = 2, y = 2, yend = 2), 
+  geom_segment(aes(x = 1, xend = 2, y = 2, yend = 3), 
                color = "red", linetype = "solid", size = 1) +    # Second horizontal segment
   labs(
-    title = "BACI Plot showing relationship between \n parameters and variables for Pamehac",
+    title = "BACI Plot showing relationship between \n parameters and variables for Pamehac \n with biomass interaction",
     x = "Time",
     y = "Response Variable"
   ) +
   ylim(0, 7) +
   theme_minimal() +
-  annotate("text", x = 2.3, y = 2, label = "Above") +
-  annotate("text", x = 2.3, y = 5, label = "Below") +
-  annotate("text", x = 2, y = 1.5, label = "Intercept") +
-  annotate("text", x = 2, y = 4.5, label = "Intercept + type") +
-  annotate("text", x = 1, y = 6, label = "Intercept + type \n + time + trt:time") +
-  annotate("text", x = 1, y = 1.5, label = "Intercept + time")
+  annotate("text", x = 2.3, y = 3, label = "Below") +
+  annotate("text", x = 2.3, y = 5, label = "Above") +
+  annotate("text", x = 2, y = 2, label = "Intercept + \n type") +
+  annotate("text", x = 2, y = 4.5, label = "Intercept") +
+  annotate("text", x = 1, y = 5.5, label = "Intercept + time") +
+  annotate("text", x = 1, y = 3, label = "Intercept + type \n + time + trt:time")
 
-ggsave("output/plot_baci_explained.png")
+ggsave("output/plot_baci_explained.png", width=10, height=8, units="in")
